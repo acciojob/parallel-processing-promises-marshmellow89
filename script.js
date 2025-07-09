0b351ba3f9a353b3ff37a9a521f81454b61d2f35
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-09 07:01:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+document.getElementById("loading").innerText = "loading"
+function imgDownload(){
+	const allimgs = images.map(image => 
+		fetch(image.url)
+			.then(res => {
+				if(!res.ok){
+					throw new Error("Failed to load image:" + image.url);
+				}
+				return res.url;
+			}).then(url => {
+				const img = document.createElement("img");
+				img.src = url;
+				getElementById("output").appendchild(img);
+			});				   
+
+							   
+	);
+	document.getElementById("loading").innerText = ""
+
+	Promise.all(allimgs)
+	   .then(() => {
+		   console.log("all img downloaded");
+	   }).catch(err => {
+		   const errorDiv = document.getElementById("error");
+		   errorDiv.innerText = err.message;
+	   });
+}
